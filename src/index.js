a5c1b95952b485694d5aa98e3b2ce7b6e6dfe25a
morgannegagne <morgannegagne@gmail.com>
2018-03-05 10:57:27 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-const theatreId = 1;
+const theatreId = 7;
+const baseApi = 'https://evening-plateau-54365.herokuapp.com/theatres'
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("DOM content loaded");
+  let app = new App()
+});
