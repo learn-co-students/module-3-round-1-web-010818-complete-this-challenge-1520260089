1083f6417cd9f492bf05956da952755281793f82
katycarr <khcarr9@gmail.com>
2018-03-05 10:57:45 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-const theatreId = 1;
+const theatreId = 5;
+const baseUrl = 'https://evening-plateau-54365.herokuapp.com/'
+
+document.addEventListener('DOMContentLoaded', event => {
+  let app = new App();
+  app.fetchShowings();
+})
